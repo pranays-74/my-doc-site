3a3dcbedbda8b18caf1da56497195a5d2904616f
pranays-74 <pranayshende74@outlook.com>
2025-01-24 13:21:42 +0530

    org change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://pranays.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-doc-site',
+  baseUrl: '/my-doc-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
