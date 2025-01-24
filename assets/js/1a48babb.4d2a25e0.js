be5a281ba5fa03bb726261d90dc15d0099071473
pranays-74 <pranayshende74@outlook.com>
2025-01-24 13:07:33 +0530

    Deploy website - based on a3f74a8e3c97c9ac6dcc82f4a8cdbb70c5f6eb60

---
+++ assets/js/1a48babb.4d2a25e0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_doc_site=self.webpackChunkmy_doc_site||[]).push([[670],{9593:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/my-doc-site/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.772b3a5b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_doc_site=self.webpackChunkmy_doc_site||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/my-doc-site/blog","blogTitle":"Blog","authorsListPath":"/my-doc-site/blog/authors"}')}}]);
